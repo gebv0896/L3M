e53d6e284c25b78b0aa025663ddbfa5960bd70e7
GABRIEL <GABRIEL@GABRIEL>
2019-08-13 12:55:07 -0600

    Pruebas

---
+++ NOSIRVE/Controllers/L3MController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using NOSIRVE.Models;
+
+namespace NOSIRVE.Controllers
+{
+    public class L3MController : ApiController
+    {
+        private ProductosDbContext db = new ProductosDbContext();
+
+        // GET: api/L3M
+        public IQueryable<Producto> GetProductos()
+        {
+            return db.Productos;
+        }
+
+        // GET: api/L3M/5
+        [ResponseType(typeof(Producto))]
+        public IHttpActionResult GetProducto(int id)
+        {
+            Producto producto = db.Productos.Find(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(producto);
+        }
+
+        // PUT: api/L3M/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProducto(int id, Producto producto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != producto.ID_prod)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(producto).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/L3M
+        [ResponseType(typeof(Producto))]
+        public IHttpActionResult PostProducto(Producto producto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Productos.Add(producto);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ProductoExists(producto.ID_prod))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = producto.ID_prod }, producto);
+        }
+
+        // DELETE: api/L3M/5
+        [HttpDelete]
+        [Route("api/L3M/{id}")]
+        [ResponseType(typeof(Producto))]
+        public IHttpActionResult DeleteProducto(int id)
+        {
+            Producto producto = db.Productos.Find(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            db.Productos.Remove(producto);
+            db.SaveChanges();
+
+            return Ok(producto);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductoExists(int id)
+        {
+            return db.Productos.Count(e => e.ID_prod == id) > 0;
+        }
+    }
+}
---
+++ NOSIRVE/Models/Producto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NOSIRVE.Models
+{
+    public class Producto
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ID_prod { get; set; }
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public int UnidadesEnAlmacen { get; set; }
+    }
+
+    class ProductosDbContext : DbContext
+    {
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
